948cb8ff6bad6e29d6c4daf06103df6945c6e894
Aspvpboss <b.f.vaughan2026@gmail.com>
2025-11-03 12:37:55 -0500

    [SDK1 - 23]: testing the test, forgot to add the default font

--- library_development/main.c
+++ library_development/main.c
@@ -1,6 +1,14 @@
 #include "SDK.h"
 
+void update_text(SDK_TextDisplay *text, double fps){
 
+    static char fps_text[20];
+
+    snprintf(fps_text, sizeof(fps_text), "%.2f", fps);
+
+    SDK_Text_UpdateString(text, fps_text);
+
+}
 
 int main(){
 
@@ -9,6 +17,12 @@ int main(){
     SDK_Display *display = SDK_CreateDisplay("SDK window", 400, 400, SDL_WINDOW_MAXIMIZED);
     SDK_Time *time = SDK_CreateTime(500);
     SDK_Input *input = SDK_CreateInput();
+    SDK_TextDisplay *text = SDK_CreateText(display, NULL, 20, 5, 5, (SDL_Color){255, 255, 255, 255});
+
+    if(!text){
+        printf("Kys!\n");
+        return 1;
+    }
 
     bool running = true;
     SDL_Event e;
@@ -16,9 +30,9 @@ int main(){
     while(running){
 
         while(SDL_PollEvent(&e)){
+
             if(e.type == SDL_EVENT_QUIT){
                 running = false;
-                break;
             }
                 
         }
@@ -27,11 +41,21 @@ int main(){
             running = false;
         }
 
+        if(time->fps_updated)
+            update_text(text, time->fps);
+
+        //SDK_Text_Render(text);
         SDK_TimeFunctions(time);
         SDK_Update_Previous_Inputs(input);
+
+        SDL_RenderClear(display->renderer);
+        SDL_RenderPresent(display->renderer);
     }
 
-    SDK_DestroyDisplay(display);
+    //SDK_DestroyDisplay(display);
+    //SDK_DestroyInput(input);
+    //SDK_DestroyTime(time);
+    //SDK_DestroyText(text);
 
     return 0;
 }
