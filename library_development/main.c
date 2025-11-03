41c6491c915ae98771973d01b63b6483c8d7c079
Aspvpboss <b.f.vaughan2026@gmail.com>
2025-11-03 10:20:48 -0500

    [SDK1 - 22]: the test is working, memtrack is not printing info though

--- library_development/main.c
+++ library_development/main.c
@@ -6,5 +6,32 @@ int main(){
 
     SDK_Init();
 
+    SDK_Display *display = SDK_CreateDisplay("SDK window", 400, 400, SDL_WINDOW_MAXIMIZED);
+    SDK_Time *time = SDK_CreateTime(500);
+    SDK_Input *input = SDK_CreateInput();
+
+    bool running = true;
+    SDL_Event e;
+
+    while(running){
+
+        while(SDL_PollEvent(&e)){
+            if(e.type == SDL_EVENT_QUIT){
+                running = false;
+                break;
+            }
+                
+        }
+
+        if(SDK_Keyboard_JustPressed(input, SDL_SCANCODE_ESCAPE)){
+            running = false;
+        }
+
+        SDK_TimeFunctions(time);
+        SDK_Update_Previous_Inputs(input);
+    }
+
+    SDK_DestroyDisplay(display);
+
     return 0;
 }
