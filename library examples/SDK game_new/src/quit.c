7d8afa059ebf7d8ed6b1577f9dc2396f92ed8536
Aspvpboss <b.f.vaughan2026@gmail.com>
2025-11-07 09:43:44 -0500

    2.3 - minor changes to quit.c, still a segfault issue

--- library examples/SDK game_new/src/quit.c
+++ library examples/SDK game_new/src/quit.c
@@ -3,14 +3,12 @@
 void free_sprites(Appstate *state){
 
     for(int i = 0; i < state->a_sprite_manager.amount_sprites; i++){
-        if(!state->a_sprite_manager.amount_sprites) break;
         SDK_DestroySprite(state->a_sprite_manager.sprites[i]);
         state->a_sprite_manager.sprites[i] = NULL;
     }
     state->a_sprite_manager.sprites = NULL;
 
     for(int i = 0; i < state->s_sprite_manager.amount_sprites; i++){
-        if(!state->s_sprite_manager.amount_sprites) break;
         SDK_DestroySprite(state->s_sprite_manager.sprites[i]);
         state->s_sprite_manager.sprites[i] = NULL;
     }
@@ -37,12 +35,12 @@ void SDL_AppQuit(void *appstate, SDL_AppResult result){
 
     Appstate *state = (Appstate*)(appstate);
 
-    SDK_DestroyDisplay(state->display);
-    // if(state->input)
-    //     SDK_DestroyInput(state->input);
-    // SDK_DestroyTime(state->time);
     free_sprites(state);
     free_text_manager(state);
+    SDK_DestroyDisplay(state->display);
+    SDK_DestroyInput(state->input);
+    SDK_DestroyTime(state->time);
+
 
     SDK_Quit();
 
